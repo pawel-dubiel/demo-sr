ec6c09bf0079e19d0117b86ee862302b7adcb502
paweldubiel <contact@paweldubiel.com>
2023-09-17 16:44:42 +0200

    GameTest remove public

--- src/test/java/com/paweldubiel/demo/scoreboard/GameTest.java
+++ src/test/java/com/paweldubiel/demo/scoreboard/GameTest.java
@@ -9,7 +9,7 @@ class GameTest {
 
   @Test
   @DisplayName("Should calculate total score correctly")
-  public void testGameTotalScore() {
+  void testGameTotalScore() {
     // given
     Game game = new Game("TeamA", "TeamB", 3, 2, 1L);
 
@@ -22,7 +22,7 @@ class GameTest {
 
   @Test
   @DisplayName("Should throw exception for negative scores")
-  public void testNegativeScores() {
+  void testNegativeScores() {
     // Assert
     assertThrows(IllegalArgumentException.class, () -> new Game("TeamA", "TeamB", -1, 2, 1L));
     assertThrows(IllegalArgumentException.class, () -> new Game("TeamA", "TeamB", 3, -1, 1L));
